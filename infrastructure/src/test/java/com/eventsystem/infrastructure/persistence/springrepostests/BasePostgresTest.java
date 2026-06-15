e1a25749cb84eed817372b09b7859181db93a6ec
Benik12 <veitsbin@post.bgu.ac.il>
2026-06-16 01:14:12 +0300

    solved the bug with CI

--- infrastructure/src/test/java/com/eventsystem/infrastructure/persistence/springrepostests/BasePostgresTest.java
+++ infrastructure/src/test/java/com/eventsystem/infrastructure/persistence/springrepostests/BasePostgresTest.java
@@ -3,18 +3,23 @@ package com.eventsystem.infrastructure.persistence.springrepostests;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 
 public abstract class BasePostgresTest {
 
     // הקונטיינר הסטטי המשותף לכולם
+    @Container
     static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
-            .withDatabaseName("eventsystem_db")
+            .withDatabaseName("testdb")
             .withUsername("myuser")
             .withPassword("mypassword");
 
     // מפעיל את הקונטיינר פעם אחת בלבד לפני תחילת כל הטסטים
     static {
-        postgres.start();
+        // String skipDocker = System.getenv("SKIP_DOCKER");
+        // if (skipDocker == null || !skipDocker.equals("true")) {
+            postgres.start();
+        // }
     }
 
     @DynamicPropertySource
